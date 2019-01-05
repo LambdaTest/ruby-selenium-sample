f63e56c8645f6e85e5a4bb826aee06e35454d496
nikhil-lambda <nikhily@lambdatest.com>
2019-01-05 18:07:49 +0530

    docs updated.

--- google-serach-lambdatest.rb
+++ google-serach-lambdatest.rb
@@ -7,14 +7,26 @@ LambdaTest selenium automation sample example
 Configuration
 ----------
 username: Username can be found at automation dashboard
-accessToken:  AccessToken can be genarated from automation dashboard or profile section
+accessToken:  AccessToken can be generated from automation dashboard or profile section
 
 Result
 -------
-Execute Test on lambdatest Distributed Grid perform selenium automation based 
+Execute Ruby Automation Tests on LambdaTest Distributed Selenium Grid 
 """
 class LtTest < Test::Unit::TestCase
+    
     def setup
+        """
+        Setup remote driver
+        Params
+        ----------
+        platform : Supported platform - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
+        browserName : Supported platform - (chrome, firefox, Internet Explorer, MicrosoftEdge)
+        version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
+
+        Result
+        -------
+        """
         username= "{username}"
         accessToken= "{accessToken}"
         gridUrl = "beta-hub.lambdatest.com/wd/hub"
@@ -41,6 +53,15 @@ class LtTest < Test::Unit::TestCase
 
 
     def test_Login
+        """
+        Setup remote driver
+        Params
+        ----------
+        Execute test:  navigate google.com search LambdaTest
+        Result
+        -------
+        print title
+        """
         puts("Searching lambdatest on google.com ")
         sleep(15)
         @driver.get("https://www.google.com/ncr")
@@ -54,6 +75,9 @@ class LtTest < Test::Unit::TestCase
   
  
     def teardown
+        """
+        Quit selenium driver
+        """
         @driver.quit
     end
  
